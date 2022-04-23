59ee72902083f77106bcda7f0a7586819caafca2
Pasha <nps-11@mail.ru>
2022-04-23 18:16:25 +0300

    обновил

--- src/pages/my-plants/PlantListPage.tsx
+++ src/pages/my-plants/PlantListPage.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 
-import {
-  Box,
-  CircularProgress,
-  Skeleton,
-  Stack,
-  Typography,
-} from '@mui/material'
+import AddIcon from '@mui/icons-material/AddRounded'
+import { Box, Button, Fab, Skeleton, Stack, Typography } from '@mui/material'
 import { useQuery } from 'react-query'
 
-import { getMyPlants } from '../../api'
-import PlantCardItem from '../../components/PlantCardItem'
-import withProtectedRoute from '../../components/withProtectedRoute'
-import { useUser } from '../../hooks'
+import { getMyPlants } from '@lulz/api'
+import PlantCardItem from '@lulz/components/PlantCardItem'
+import withProtectedRoute from '@lulz/components/withProtectedRoute'
+import { useUser } from '@lulz/hooks'
 
 export const PlantListPage = () => {
   const { user } = useUser()
@@ -56,11 +51,25 @@ export const PlantListPage = () => {
   }
 
   return (
-    <Stack spacing={1}>
-      {data?.map(plant => (
-        <PlantCardItem flower={plant} key={plant.id} />
-      ))}
-    </Stack>
+    <Box position="relative">
+      <Button startIcon={<AddIcon />}>Новое растение</Button>
+      <Stack spacing={1}>
+        {data?.map(plant => (
+          <PlantCardItem flower={plant} key={plant.id} />
+        ))}
+      </Stack>
+      <Fab
+        color="secondary"
+        aria-label="add"
+        sx={{
+          position: 'absolute',
+          right: 16,
+          bottom: 16,
+        }}
+      >
+        <AddIcon />
+      </Fab>
+    </Box>
   )
 }
 
