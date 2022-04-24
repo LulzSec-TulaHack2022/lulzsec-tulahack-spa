af32eca4c2bd55a327a8dded9aa66b63c36902b5
Pasha <nps-11@mail.ru>
2022-04-24 12:56:35 +0300

    Добавил красоты

--- src/api/plant-delete.service.ts
+++ src/api/plant-delete.service.ts
@@ -2,7 +2,7 @@ import { MyFlower } from '../types'
 
 import { API } from './API'
 
-export const deletePlant = (flowerId: string): Promise<MyFlower[]> =>
+export const deletePlant = (flowerId: string | number): Promise<MyFlower[]> =>
   API.delete('/flowers', {
     data: {
       flower_id: flowerId,
